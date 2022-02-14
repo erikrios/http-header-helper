db949f061260c943e2c2bb83121a06627e7d5113
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-02-14 07:34:20 +0700

    Add a function to check if the http request header contains basic authentication schema

--- http_header_helper.go
+++ http_header_helper.go
@@ -1,1 +1,24 @@
 package httpheaderhelper
+
+import (
+	"net/http"
+	"strings"
+)
+
+// IsContainsBasicAuthenctication check the authentication schema.
+// Return true if the schema is valid.
+// Otherwise, return false.
+func IsContainsBasicAuthenctication(r *http.Request) bool {
+	stringValue := r.Header.Get("Authorization")
+	values := strings.Split(stringValue, " ")
+
+	if len(values) != 2 {
+		return false
+	}
+
+	if values[0] != "Basic" {
+		return false
+	}
+
+	return true
+}
